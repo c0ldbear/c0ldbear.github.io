4980bc4c46dfda331684f48b3de520e7ff7006f0
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2023-11-08 09:49:01 +0100

    Move creation of json to own function

--- scripts/wsg-info.rb
+++ scripts/wsg-info.rb
@@ -6,37 +6,40 @@ def get_wsg_info
     url = "https://raw.githubusercontent.com/w3c/sustyweb/main/guidelines.json"
     response = HTTParty.get(url)
     result = ""
-    json_obj = {}
-    result_json = []
 
     if response.code == 200
-        wsg_json = JSON.parse(response.body)
-
-        wsg_json[Constant::CATEGORY].each do |category|
-            if !category["guidelines"].nil? 
-                section_id = category[Constant::ID]
-                section_name = category[Constant::NAME]
-                category["guidelines"].each do |guideline|
-                    json_obj[Constant::TITLE] = section_id + "." + guideline[Constant::ID].to_s + " " + guideline["guideline"]
-                    json_obj[Constant::CATEGORY] = {
-                        Constant::ID => section_id,
-                        Constant::TITLE => section_name
-                    }
-                    json_obj[Constant::DESCRIPTION] = guideline[Constant::DESCRIPTION]
-                    json_obj[Constant::IMPACT] = guideline[Constant::IMPACT]
-                    json_obj[Constant::EFFORT] = guideline[Constant::EFFORT]
-                    json_obj[Constant::TAGS] = guideline[Constant::TAGS]
-                    result_json.append(json_obj)
-                    json_obj = {}
-                end
-            end
-        end
-
-        result = result_json 
+        result = create_wsg_json(JSON.parse(response.body))
     else
         puts "Failed! #{response.code}"
     end
     result
+end
+
+def create_wsg_json(wsg_json)
+    json_obj = {}
+    result_json = []
+
+    wsg_json[Constant::CATEGORY].each do |category|
+        if !category["guidelines"].nil? 
+            section_id = category[Constant::ID]
+            section_name = category[Constant::NAME]
+            category["guidelines"].each do |guideline|
+                json_obj[Constant::TITLE] = section_id + "." + guideline[Constant::ID].to_s + " " + guideline["guideline"]
+                json_obj[Constant::CATEGORY] = {
+                    Constant::ID => section_id,
+                    Constant::TITLE => section_name
+                }
+                json_obj[Constant::DESCRIPTION] = guideline[Constant::DESCRIPTION]
+                json_obj[Constant::IMPACT] = guideline[Constant::IMPACT]
+                json_obj[Constant::EFFORT] = guideline[Constant::EFFORT]
+                json_obj[Constant::TAGS] = guideline[Constant::TAGS]
+                result_json.append(json_obj)
+                json_obj = {}
+            end
+        end
+    end
+
+    return result_json
 end
 
 class Constant
