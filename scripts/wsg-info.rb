02e5a37633dc1f8ebd3ada2f0e5e8562a1ba0f3f
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2023-11-08 10:10:12 +0100

    Add a function to generate links to each topic

--- scripts/wsg-info.rb
+++ scripts/wsg-info.rb
@@ -24,7 +24,9 @@ def create_wsg_json(wsg_json)
             section_id = category[Constant::ID]
             section_name = category[Constant::NAME]
             category["guidelines"].each do |guideline|
-                json_obj[Constant::TITLE] = section_id + "." + guideline[Constant::ID].to_s + " " + guideline["guideline"]
+                topic = guideline["guideline"]
+                title = section_id + "." + guideline[Constant::ID].to_s + " " + topic
+                json_obj[Constant::TITLE] = title
                 json_obj[Constant::CATEGORY] = {
                     Constant::ID => section_id,
                     Constant::TITLE => section_name
@@ -33,13 +35,21 @@ def create_wsg_json(wsg_json)
                 json_obj[Constant::IMPACT] = guideline[Constant::IMPACT]
                 json_obj[Constant::EFFORT] = guideline[Constant::EFFORT]
                 json_obj[Constant::TAGS] = guideline[Constant::TAGS]
+                json_obj[Constant::URL] = generate_topic_links(topic)
                 result_json.append(json_obj)
                 json_obj = {}
             end
         end
     end
 
-    return result_json
+    result_json
+end
+
+def generate_topic_links(topic)
+    base_url = Constant::BASEURL
+
+    topic_anchor = "#" + topic.downcase.gsub(/,|'|\s|\(|\)/, '' => '', '\'' => '-', ' ' => '-')
+    topic_link = base_url + topic_anchor
 end
 
 class Constant
@@ -53,4 +63,6 @@ class Constant
     EFFORT = "effort"
     TAGS = "tags"
     FILENAME = "wsg-info"
+    BASEURL = "https://w3c.github.io/sustyweb/"
+    URL = "url"
 end
